2b75639fba88e272c48630af3e7028c80c701d26
Raghu Betina <rbetina@gmail.com>
2016-05-18 10:51:04 -0500

    Generate List resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518155007) do
+ActiveRecord::Schema.define(version: 20160518155049) do
 
   create_table "boards", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "board_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
