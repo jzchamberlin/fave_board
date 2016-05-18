05562e4f48c38ebf5128e32474e183cbd54bd19e
Raghu Betina <rbetina@gmail.com>
2016-05-18 11:47:43 -0500

    Add destroy callbacks

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,5 @@
 class Board < ActiveRecord::Base
   validates :name, :presence => true
 
-  has_many :lists, :class_name => "List", :foreign_key => "board_id"
+  has_many :lists, :class_name => "List", :foreign_key => "board_id", :dependent => :destroy
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -4,5 +4,5 @@ class List < ActiveRecord::Base
 
   belongs_to :board, :class_name => "Board", :foreign_key => "board_id"
 
-  has_many :cards, :class_name => "Card", :foreign_key => "list_id"
+  has_many :cards, :class_name => "Card", :foreign_key => "list_id", :dependent => :destroy
 end
