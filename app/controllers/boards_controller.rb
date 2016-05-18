14597b6d46279a8d104cae92d17f4c8b835a8010
Raghu Betina <rbetina@gmail.com>
2016-05-18 10:50:20 -0500

    Generate Board resource

---
+++ app/controllers/boards_controller.rb
@@ -0,0 +1,50 @@
+class BoardsController < ApplicationController
+  def index
+    @boards = Board.all
+  end
+
+  def show
+    @board = Board.find(params[:id])
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    @board = Board.new
+    @board.user_id = params[:user_id]
+    @board.name = params[:name]
+
+    if @board.save
+      redirect_to "/boards", :notice => "Board created successfully."
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:id])
+
+    @board.user_id = params[:user_id]
+    @board.name = params[:name]
+
+    if @board.save
+      redirect_to "/boards", :notice => "Board updated successfully."
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @board = Board.find(params[:id])
+
+    @board.destroy
+
+    redirect_to "/boards", :notice => "Board deleted."
+  end
+end
