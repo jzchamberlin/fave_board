456f4ad4827a3deddb6ba7c2cedebc8afb485103
Raghu Betina <rbetina@gmail.com>
2016-05-18 12:53:46 -0500

    Add association helper methods for users and boards

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,5 +1,7 @@
 class Board < ActiveRecord::Base
   validates :name, :presence => true
 
+  belongs_to :user
+  
   has_many :lists, :class_name => "List", :foreign_key => "board_id", :dependent => :destroy
 end
