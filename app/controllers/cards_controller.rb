61541a868a63d232c2ab66e644dbf39477b9f93f
Raghu Betina <rbetina@gmail.com>
2016-05-18 12:21:45 -0500

    Improve cards#create redirect behavior

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -18,7 +18,7 @@ class CardsController < ApplicationController
     @card.list_id = params[:list_id]
 
     if @card.save
-      redirect_to "/cards", :notice => "Card created successfully."
+      redirect_to :back, :notice => "Card created successfully."
     else
       render 'new'
     end
